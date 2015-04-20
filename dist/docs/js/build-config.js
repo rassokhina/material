16d9df6b9774bdc81280d94a72c9106e865c9789
rassokhina <rassokhinanastya@mail.ru>
2015-04-20 17:59:26 +0600

    fix UI design

--- dist/docs/js/build-config.js
+++ dist/docs/js/build-config.js
@@ -2,6 +2,6 @@ DocsApp.constant('BUILDCONFIG', {
   "ngVersion": "1.3.2",
   "version": "0.8.3",
   "repository": "https://github.com/angular/material",
-  "commit": "a8af2454d6a520d97c73f35b1ba7f5b9e792c413",
-  "date": "2015-03-31 12:59:00 +0600"
+  "commit": "0db040d605cfe16bd569bf0e79e2d68e8e4a7f29",
+  "date": "2015-04-16 12:14:48 +0600"
 });
